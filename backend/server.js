2f06723b7d7b8ff5c56b4e346744f03cc8d99f61
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-09-13 20:58:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use("/uploads", express.static(path.join(__dirname, "../public/uploads")));
 
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("/*", (req, res) => {
+app.get(/.*/, (req, res) => {
   res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
 });
 
