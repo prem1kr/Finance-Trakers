04ac944eea7f4036e82082899ea0f673f072d61d
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-09-13 17:43:05 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173", 
+  origin: "https://personal-finance-traker.onrender.com", 
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 }));
