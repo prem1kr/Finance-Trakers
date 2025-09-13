c2e12deccec24bdb3b2e7c175a215e6df08e59b7
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-09-13 20:40:14 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,7 @@ app.use("/uploads", express.static(path.join(process.cwd(), "public/uploads")));
 app.use(express.static(path.join(process.cwd(), "frontend", "dist")));
 
 // Catch-all route to serve index.html for SPA client-side routing
-app.get("frontend/dist/index.html", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(process.cwd(), "frontend", "dist", "index.html"));
 });
 
